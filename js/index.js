ac157899edc27aae7a729a3f420fee94a947ad43
Jose Jimenez <jimenez.fx@outlook.com>
2023-11-13 13:25:17 -0600

    Cambios

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-function change_content(a){switch(change_active(a),$("html, body").animate({scrollTop:0},"slow"),a){case"about":default:$("#main").load("./components/home.html");break;case"works":$("#main").load("./components/works.html");break;case"contact":$("#main").load("./components/contact.html")}}function change_active(a){$("ul li").removeClass("active"),$("#"+a).addClass("active")}document.addEventListener("DOMContentLoaded",()=>{$("#main").load("./components/home.html"),$("#about").addClass("active")});
+function change_content(a) {
+    switch ((change_active(a), $("html, body").animate({ scrollTop: 0 }, "slow"), a)) {
+        case "about":
+        default:
+            $("#main").load("./components/home.html");
+            break;
+        case "works":
+            $("#main").load("./components/works.html");
+            clearInterval(interval1);
+            clearInterval(interval2);
+            break;
+        case "contact":
+            $("#main").load("./components/contact.html");
+    }
+}
+function change_active(a) {
+    $("ul li").removeClass("active"), $("#" + a).addClass("active");
+}
+document.addEventListener("DOMContentLoaded", () => {
+    $("#main").load("./components/home.html"), $("#about").addClass("active");
+});
--- js/upload_images.js
+++ js/upload_images.js
@@ -1,1 +1,29 @@
-var photos_wide=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"],photos_long=["1","2","3","4","5","6","7","8","9","10","11"],url_web_long="./img/photography/long/",url_web_wide="./img/photography/Wide/";function auto_wide(){rellenar("container_image_wide",url_web_wide+getnumber(20)+".webp"),setInterval(()=>{document.getElementById("container_image_wide").classList.remove("show_off"),rellenar("container_image_wide",url_web_wide+getnumber(20)+".webp")},31e3)}function auto_long(){rellenar("container_image_long",url_web_long+getnumber(11)+".webp"),setInterval(()=>{document.getElementById("container_image_long").classList.remove("show_off"),rellenar("container_image_long",url_web_long+getnumber(11)+".webp")},3e4)}function getnumber(e){return Math.floor(Math.random()*e)+1}async function rellenar(e,n){var o=document.getElementById(e);o.classList.add("show_off");var r=document.createElement("img");r.src=n,r.classList.add("lazyload"),o.innerHTML=null,o.append(r)}auto_wide(),auto_long();
+var photos_wide = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+    photos_long = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
+    url_web_long = "./img/photography/long/",
+    url_web_wide = "./img/photography/Wide/";
+function auto_wide() {
+    rellenar("container_image_wide", url_web_wide + getnumber(20) + ".webp"),
+            document.getElementById("container_image_wide").classList.remove("show_off"), rellenar("container_image_wide", url_web_wide + getnumber(20) + ".webp");
+}
+function auto_long() {
+    rellenar("container_image_long", url_web_long + getnumber(11) + ".webp"),
+      document.getElementById("container_image_long").classList.remove("show_off"), rellenar("container_image_long", url_web_long + getnumber(11) + ".webp");
+}
+function getnumber(e) {
+    return Math.floor(Math.random() * e) + 1;
+}
+async function rellenar(e, n) {
+    var o = document.getElementById(e);
+    o.classList.add("show_off");
+    var r = document.createElement("img");
+    (r.src = n), r.classList.add("lazyload"), (o.innerHTML = null), o.append(r);
+}
+auto_wide(), auto_long();
+// const interval1 = setInterval(() => {
+//     auto_wide();
+// }, 1000);
+// const interval2 =setInterval(() => {
+//   auto_long();
+// }, 1000);
+
